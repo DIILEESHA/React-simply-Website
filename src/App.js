9a92ba4f47cf383584ee6b1fcd1598632eaf835d
DILEESHA1 <dileeshanawarathna18@gmail.com>
2021-11-19 15:32:36 +0530

    addme1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     hello me world mmmmmmmmmmmmmmmddddddmmmvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvsss
+    </div>
+  );
+}
+
+export default App;
+0
---
+++ src/components/ger.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ger = () => {
+    return (
+        <div>
+            getter
+        </div>
+    )
+}
+
+export default ger
